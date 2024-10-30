12c5d328a624e9c5ffb9a99e392482227258fbc8
KadriGit <kadri.viira@tktk.ee>
2024-10-30 17:52:37 +0200

    final code

--- week5AndOperator/Program.cs
+++ week5AndOperator/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// AND (&&) and OR () logical opererator
+
+//AND 
+// true && true ==> true
+// true && false ==> false
+// false && true ==> false
+// false && false ==> false
+
+string username, password; // username: admin ; password admin: admin1234
+
+Console.WriteLine("Enter username: ");
+username =Console.ReadLine();
+
+Console.WriteLine("Enter password: ");
+password = Console.ReadLine();
+
+if (username == "admin" && password == "admin1234")
+{
+    Console.WriteLine("Welcome!");
+}
+else
+{
+    Console.WriteLine("Invalid user credential");
+}
